9fb0f1e964142bf99b2152b6f8581fb5c9b6bcef
Jedd Wong <jedd@Jedds-MacBook-Pro-16.local>
2021-04-21 22:02:19 +0800

    Added img to pdf function

--- Qunzip.py
+++ Qunzip.py
@@ -1,24 +1,39 @@
-
 import os
 from os import listdir
 from zipfile import ZipFile
+import img2pdf
 
+#Get Current Working Directory
 zipfolder = os.getcwd()
 
-for zipname in os.listdir(zipfolder):
-    zip_name = zipfolder+zipname
-    clean_name = os.path.splitext(zipname)[0]
+# read all zip files in folder
+for zip_files in os.listdir(zipfolder):
+    if not zip_files.endswith(".zip"):
+        continue
+    # zip_name = zipfolder+zip_files #應該冇用⋯
+    clean_name = os.path.splitext(zip_files)[0]
+    dirname = zipfolder+'/'+clean_name
 
-    if zipname.endswith('.zip'):
+# create a folder by zip_files's name
+    if zip_files.endswith('.zip'):
         os.makedirs(clean_name)
+# 把zip files的圖片unzip到上面建立的folder內
+    if zip_files.endswith('.zip'):
+        with ZipFile(zip_files, 'r') as zipObj:
+            zipObj.extractall(dirname)
 
-    if zipname.endswith('.zip'):
-        with ZipFile(zipname, 'r') as zipObj:
-            zipObj.extractall(zipfolder+'/'+clean_name)
-
-    list = os.listdir(zipfolder+'/'+clean_name)
-    new_list = [x for x in list if x.endswith(".jpg")]
-    print("Total number of images to convert is: ", len(new_list))
+# 成功，但未能放在圖片新開的folder內
+    with open(f"{clean_name}.pdf", "wb") as f:
+        imgs = []
+        for pdf_files in os.listdir(dirname):
+            if not pdf_files.endswith(".jpg"):
+                continue
+            path = os.path.join(dirname, pdf_files)
+            if os.path.isdir(path):
+                continue
+            imgs.append(path)
+            imgs.sort()
+        f.write(img2pdf.convert(imgs))
 
 # waiting to fix '.DS_Store' problem in Mac
 # can using `rm .DS_Store` to detele this file first then run these code
